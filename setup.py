ae6dc7f6d39a37d79f0e2fbc061fa0071355aa7e
Michael Neuhold <S1810307094@students.fh-hagenberg.at>
2022-11-21 23:33:39 +0100

    increased version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name='bdvlib',
-    version='0.1.3',
+    version='0.1.4',
     author='Michael Neuhold',
     author_email='S2110454008@students.fh-hagenberg.at',
     description='...',
